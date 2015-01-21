18dd90b134379438a1effb7aed24272b48a251be
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2015-01-21 21:10:20 +0300

    error handling

--- command.go
+++ command.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
+	if len(os.Args) != 3 {
+		fmt.Printf("usage: %s \"first phrase\" \"second phrase\"\n", os.Args[0])
+		os.Exit(1)
+	}
 
-	first := args[0]
-	second := args[1]
+	first := os.Args[1]
+	second := os.Args[2]
 
 	fmt.Printf("\"%s\" - %d results\n", first, 10000)
 	fmt.Printf("\"%s\" - %d results\n", second, 100000)
