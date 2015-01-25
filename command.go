c0133d7cf782e40c7acc3916af36e7d33535ea1b
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2015-01-25 12:17:24 +0300

    read response

--- command.go
+++ command.go
@@ -2,7 +2,8 @@ package main
 
 import (
 	"fmt"
-
+	"io/ioutil"
+	"net/http"
 	"os"
 )
 
@@ -14,6 +15,23 @@ func main() {
 
 	first := os.Args[1]
 	second := os.Args[2]
+
+	request := fmt.Sprintf("http://www.google.com/trends/fetchComponent?q=%s,%s&cid=TIMESERIES_GRAPH_0&export=3", first, second)
+	fmt.Println(request)
+	resp, err := http.Get(request)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s\n", string(contents))
 
 	fmt.Printf("\"%s\" - %d results\n", first, 10000)
 	fmt.Printf("\"%s\" - %d results\n", second, 100000)
